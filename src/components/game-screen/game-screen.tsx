405c33f090022143457689fb4fcfae5cdd9c7aed
Matt Newbigging <mrnewbigging@gmail.com>
2025-10-25 20:15:11 +0100

    button active box shadow

--- src/components/game-screen/game-screen.tsx
+++ src/components/game-screen/game-screen.tsx
@@ -1,5 +1,6 @@
 import { appState } from "../../app-state/app-state";
 import { GameState } from "../../game/game-state";
+import { useEventUpdater } from "../hooks/use-event-updater";
 import { BuildMenu } from "./build-menu/build-menu";
 import "./game-screen.scss";
 
@@ -8,16 +9,23 @@ interface GameScreenProps {
 }
 
 export function GameScreen({ gameState }: GameScreenProps) {
+  useEventUpdater("toggled-build-menu");
+
+  const buildActiveClass = appState.showBuildMenu ? "active" : "";
+
+  const buildBtnClasses = ["button", buildActiveClass].join(" ");
+
   return (
     <>
       {/* These menus are absolutely positioned, outside normal flow */}
       <BuildMenu />
 
+      {/* Back to normal flow: */}
       <div className="game-screen">
         <div className="no-touch"></div>
         <div className="bottom-bar-area">
           <div
-            className="button"
+            className={buildBtnClasses}
             data-tooltip="Build"
             onClick={appState.toggleBuildMenu}
           >
