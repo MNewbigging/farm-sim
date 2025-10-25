7803904e9262f46fb64e7e41b56b48e014bd58c2
Matt Newbigging <mrnewbigging@gmail.com>
2025-10-25 16:46:58 +0100

    renamed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-tester/", // replace with the repo name
+  base: "/farm-sim/",
   assetsInclude: ["**/*.gltf"],
   publicDir: "public",
   build: {
