cd791a4355064ef72c98375ab87e826a02f82667
hugolafis <hugo.newman@aize.io>
2025-11-14 14:04:38 +0000

    feat: tweak grow time

--- src/game/crops/wheat.ts
+++ src/game/crops/wheat.ts
@@ -4,7 +4,7 @@ import * as THREE from "three";
 import { CropMaterial } from "./material/crop-material";
 
 export class Wheat extends Crop {
-  protected timeToGrow = 15;
+  protected timeToGrow = 5;
   protected aridPreference = 0;
 
   constructor(tileAridness: number, tileSoilQuality: number) {
